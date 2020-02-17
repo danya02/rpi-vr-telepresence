da658e343bbe4b729418f21b745f5c61f1d1d849
Danya Generalov <danya@danya02.ru>
2020-02-17 10:38:58 +0300

    Adding squeeze input.

---
+++ unity/VR adapter/Assets/Scripts/TriggerTransmitterScript.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class TriggerTransmitterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Squeeze.AddOnChangeListener(OnLeftSqueeze, LeftHand);
+        Squeeze.AddOnChangeListener(OnRightSqueeze, RightHand);
+        GrabPinch.AddOnChangeListener(OnLeftGrabPinch, LeftHand);
+        GrabPinch.AddOnChangeListener(OnRightGrabPinch, RightHand);
+
+    }
+
+    private void OnLeftGrabPinch(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState)
+    {
+        DataManager.Send(LeftGrabKey, newState ? GrabTrueValue : GrabFalseValue);
+    }
+
+    private void OnRightGrabPinch(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState)
+    {
+        DataManager.Send(RightGrabKey, newState ? GrabTrueValue : GrabFalseValue);
+    }
+
+    private void OnLeftSqueeze(SteamVR_Action_Single fromAction, SteamVR_Input_Sources fromSource, float newAxis, float newDelta)
+    {
+        DataManager.Send(LeftSqueezeKey, newAxis.ToString());
+    }
+
+    private void OnRightSqueeze(SteamVR_Action_Single fromAction, SteamVR_Input_Sources fromSource, float newAxis, float newDelta)
+    {
+        DataManager.Send(RightSqueezeKey, newAxis.ToString());
+    }
+
+
+    public DataManagerScript DataManager;
+    public SteamVR_Action_Boolean GrabPinch;
+    public SteamVR_Action_Single Squeeze;
+    public SteamVR_Input_Sources LeftHand, RightHand;
+
+    public string LeftSqueezeKey, RightSqueezeKey, LeftGrabKey, RightGrabKey, GrabTrueValue, GrabFalseValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
